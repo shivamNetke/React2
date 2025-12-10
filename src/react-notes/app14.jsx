2cfb0fd9e48ee958071d0b7d4a3063737c8efa47
shivamNetke <netkeshiv3521@gmail.com>
2025-12-10 22:54:02 +0530

    101225

---
+++ src/react-notes/app14.jsx
@@ -0,0 +1,31 @@
+// import "./App.css";
+// import Drowpdown from "./components/Dropdown";
+
+// function App() {
+
+//   return (
+//     <>
+//       <h1>dropdown in jsx</h1>
+//       <Drowpdown />
+//     </>
+//   );
+// }
+
+// export default App;
+
+// import { useState } from "react";
+
+// function Drowpdown() {
+//     const [city, setCity] = useState("mumbai");
+//     return (
+//         <>
+//             <select onChange={(event)=>setCity(event.target.value)} defaultValue="mumbai">
+//                 <option value="pune">pune </option>
+//                 <option value="mumbai">mumbai </option>
+//                 <option value="thane">thane </option>thane
+//             </select>
+//             <h3>selected city : {city}</h3>
+//         </>
+//     )
+// }
+// export default Drowpdown;
