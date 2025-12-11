084fb7a01703a2ef324f9e39ae8159774381c2b8
shivamNetke <netkeshiv3521@gmail.com>
2025-12-11 20:01:22 +0530

    111225

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import Drowpdown from "./components/Dropdown";
 
 function App() {
 
   return (
     <>
-      <h1>dropdown in jsx</h1>
-      <Drowpdown />
+      <h2>loop in jsx with map function</h2>
+
+
     </>
   );
 }
---
+++ src/react-notes/app15.jsx
@@ -0,0 +1,76 @@
+// import "./App.css";
+
+// function App() {
+
+//   const userNames = ['shivam', 'rushikesh', 'omkar', 'joy', 'sahil', 'sarthak'];
+//   const collegeData = ['ACA', 'NACSCA', 'SCA'];
+//   const usersData = [
+
+//     {
+//       name: 'shivam netke',
+//       age: 21,
+//       email: 'shivam@gmail.com'
+//     },
+
+//     {
+//       name: 'rushikesh kokate',
+//       age: 22,
+//       email: 'rushikesh@gmail.com'
+//     },
+
+//     {
+//       name: 'omkar borde',
+//       age: 23,
+//       email: 'omkar@gmail.com'
+//     },
+
+//     {
+//       name: 'joy gajbhiv',
+//       age: 24,
+//       email: 'joy@gmail.com'
+//     },
+
+//     {
+//       name: 'sahil mate',
+//       age: 25,
+//       email: 'sahil@gmail.com'
+//     },
+
+//     {
+//       name: 'sarthak shinde',
+//       age: 26,
+//       email: 'sarthak@gmail.com'
+//     }
+
+//   ]
+
+//   return (
+//     <>
+//       <h2>loop in jsx with map function</h2>
+
+//       <table border="1">
+//         <thead>
+//           <tr>
+//             <td>name</td>
+//             <td>age</td>
+//             <td>email</td>
+//           </tr>
+//         </thead>
+
+//         <tbody>
+//           {
+//             usersData.map((user, index) => (
+//               <tr key={index}>
+//                 <td>{user.name}</td>
+//                 <td>{user.age}</td>
+//                 <td>{user.email}</td>
+//               </tr>
+//             ))
+//           }
+//         </tbody>
+//       </table>
+//     </>
+//   );
+// }
+
+// export default App;
