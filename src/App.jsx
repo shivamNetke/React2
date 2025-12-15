02d3e230f6379ff8571f8e27dd03f9e05017d195
shivamNetke <netkeshiv3521@gmail.com>
2025-12-15 19:34:38 +0530

    151225

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import CollegeDetails from "./components/CollegeDetails";
-import StudentDetails from "./components/StudentDetails";
 
 function App() {
 
-  const collegeData = [
-    {
-      name: 'ahemednagar college',
-      location: 'chandni chowk',
-      website: 'www.aca.edu.in',
-      student: [
-        {
-          name: 'shivam netke',
-          email: 'shivamnetke@gmail.com'
-        },
+  const [counter, setCounter] = useState(0);
+  const [data, setData] = useState(0)
 
-        {
-          name: 'rushikesh kokate ',
-          email: 'rushikeshkokate@gmail.com'
-        }
-      ]
-    },
+  useEffect(()=>{
+      // callOnce();
+    CounterFunction();
+  }, [counter])
 
-    {
-      name: 'sarada college',
-      location: 'patrakar chowk',
-      website: 'www.saradacollege.edu.in',
-      student: [
-        {
-          name: 'mohan padawale',
-          email: 'mohanpadawale@gmail.com'
-        },
+  function CounterFunction(params) {
+    console.log("counter function called", counter);
+    
+  }
 
-        {
-          name: 'harshal bhagat',
-          email: 'harshalbhagat@gmail.com'
-        }
-      ]
-    },
+  function callOnce(params) {
+    console.log("callOnce fun called.");
+    
+  }
 
-    {
-      name: 'new arts college',
-      location: 'delhi gate',
-      website: 'www.nacsca.edu.in',
-      student: [
-        {
-          name: 'sarthak shinde',
-          email: 'sarthakshinde@gmail.com'
-        },
-
-        {
-          name: 'vishal mane',
-          email: 'vishalmane@gmail.com'
-        }
-      ]
-    },
-
-    {
-      name: 'paulbuddhe college',
-      location: 'shriram chowk',
-      website: 'www.paulbuddhecollege.edu.in',
-      student: [
-        {
-          name: 'aniket karande',
-          email: 'aniketkarande@gmail.com'
-        },
-
-        {
-          name: 'kiran karande',
-          email: 'kirankarande@gmail.com'
-        }
-      ]
-    }
-  ]
 
   return (
     <>
-      <h1>nested loop with components</h1>
-      {
-        collegeData.map((college, index) => (
-          <div key={index}
-            style={{ backgroundColor: '#999', borderRadius: '10px', padding: '5px', margin: '40px', borderBottom: '2px solid black' }}
+      <h1>useEffect hook</h1>
+      <button onClick={()=>{setCounter(counter + 1)}}>counter {counter}</button>
+      <button onClick={()=>{setData(data + 1)}}>data {data}</button>
 
-          >
-            <CollegeDetails college={college} />
-            <StudentDetails student={college.student} />
-
-
-          </div>
-        ))
-      }
     </>
   );
 }
