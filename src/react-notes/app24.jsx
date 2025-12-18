b2b4cc5741ff1bd6dad8adb7cd57347ed2c62082
shivamNetke <netkeshiv3521@gmail.com>
2025-12-18 23:10:50 +0530

    181225

---
+++ src/react-notes/app24.jsx
@@ -0,0 +1,62 @@
+// import Card from "./components/Card";
+// import "./css/style1.css";
+// function App() {
+
+//   return (
+//     <>
+//       <h2 className="heading">external css in jsx</h2>
+
+//       <div className="container">
+
+//         {/* card start */}
+
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+      
+//         {/* card ends */}
+
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+// .heading {
+//     color: green;
+// }
+
+// .container {
+//     display: flex;
+//     flex-wrap: wrap;
+ 
+// }
+
+// .userCard {
+//     width: 14.7%;
+//     border: 1px solid #ccc;
+//     padding: 2px;
+//     border-radius: 5px;
+//     box-shadow: 1px 1px 1px 1px  lightblue;
+//     margin: 10px;
+
+// .profileImg {
+//     width: 100%;
+//     height: 70%;
+//     border-radius: 5px 5px 0px 0px;
+// }
+
+// }
+
+// .cardText {
+//     padding-left: 5%;
+// }
