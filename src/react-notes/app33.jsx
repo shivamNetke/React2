5f67270797601c938b25473cf1b8e514c4396e5d
shivamNetke <netkeshiv3521@gmail.com>
2026-01-13 20:38:20 +0530

    130126

---
+++ src/react-notes/app33.jsx
@@ -0,0 +1,42 @@
+// function DisplayUser({user}) {
+//     return (
+//         <>
+//         <h4>Display User</h4>
+//         <h6>{user}</h6>
+//         </>
+//     )
+// }
+// export default DisplayUser;
+
+// function DisplayUser({user}) {
+//     return (
+//         <>
+//         <h4>Display User</h4>
+//         <h6>{user}</h6>
+//         </>
+//     )
+// }
+// export default DisplayUser;
+
+// import './App.css';
+// import { useState } from 'react';
+// import AddUser from './components/AddUser';
+// import DisplayUser from './components/DisplayUser';
+// function App() {
+
+//     const [user, setUser] = useState('');
+
+
+//   return (
+//     <>
+//     <h1>Lifting State Up in React</h1>
+// <AddUser user={user} setUser={setUser} />
+// <DisplayUser user={user} />
+
+//     </>
+//   );
+// }
+
+
+
+// export default App;
