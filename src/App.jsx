39e9386a9a0162fd5a9a090485975e8870756a77
shivamNetke <netkeshiv3521@gmail.com>
2026-01-15 13:12:08 +0530

    150126

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,51 @@
 import { useState } from "react";
 import "./App.css";
 
+
+
 function App() {
 
-  const [data, setData] = useState({
-    name:'shivam netke',
-    address:{
-        city:'nagar',
-        pcode: 414601
-    }
-  })
+  const [data, setData] = useState(
+    ['shivam', 'ashita', 'tulsi', 'shraddha']
+  )
 
-  const handleName =(value) =>{
-    data.name = value 
-    setData({...data})
+  const handleUser = (name)=>{
+    data[data.length-1] = name;
+    console.log(data);
+    setData([...data])
   }
 
-  const handleCity = (city) =>{
-    data.address.city = city;
-    setData({...data, address:{...data.address,city}})
+  const handleUserAge = (age) =>{
+    dataDetails[dataDetails.length-1].age = age;
+    setDataDetails([...dataDetails])
   }
+  const [dataDetails, setDataDetails] = useState([
+    {name:'shivam', age:22},
+    {name:'ashita', age:21},
+    {name:'tulsi', age:20},
 
-  const handlePcode = (pcode) =>{
-    data.address.pcode = pcode;
-    setData({...data,address:{...data.address,pcode}})
-  }
+  ])
 
   return (
     <>
-      <h1>updating object in state</h1>
-      
-      <input type="text" placeholder="update name" onChange={(event)=>handleName(event.target.value)}/>
-      <input type="text" placeholder="update city" onChange={(event)=>handleCity(event.target.value)}/>
-      <input type="text" placeholder="update pcode" onChange={(event)=>handlePcode(event.target.value)}/>
+      <h2>updating array in state</h2>
+      <input type="text" placeholder="change last user" 
+      onChange={(event)=>handleUser(event.target.value)} />
+      {
+        data.map((item, index) => (
+          <h3 key={index}>{item}</h3>
+        ))
+      }
 
+      <hr />
 
-      <h2>name : {data.name}</h2>
-      <h2>city : {data.address.city}</h2>
-      <h2>pcode : {data.address.pcode}</h2>
+      <input type="text" placeholder="change last user age"
+      onChange={(event)=>handleUserAge(event.target.value)} />
+      {
+        dataDetails.map((item, index)=>(
+          <h4 key={index}>{item.name} {item.age}</h4>
+        ))
+      }
     </>
   )
 }
